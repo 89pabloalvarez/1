5914301ee6658bf5618c66d20e33488479699fef
89pabloalvarez <89.pablo.alvarez@gmail.com>
2022-04-15 19:27:33 -0300

    Framework 1.0
    Building a simple framework with a database xml, main java, methods java and application interface

---
+++ src/main/methods.java
@@ -0,0 +1,10 @@
+package main;
+
+public class methods {
+
+	public methods() {
+		// TODO Auto-generated constructor stub
+		
+	}
+
+}
---
+++ src/main/principal.java
@@ -0,0 +1,15 @@
+package main;
+
+public class principal {
+
+	public principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello world");
+
+	}
+
+}
